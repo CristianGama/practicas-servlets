5179ecd3d1879cf7eeabb217ba3083e50dfb12c5
David Torres <DAVIDTORRES_WORK@outlook.com>
2025-07-16 07:56:42 -0600

    Navbard

--- tiendaleoncito/src/App.jsx
+++ tiendaleoncito/src/App.jsx
@@ -1,11 +1,13 @@
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './index.css';
+import { Nav } from './principal/components/Nav';
 
 function App() {
   return (
     <>
       <div className='h-screen overflow-auto'>
+       <Nav/> 
       </div>
     </>
   )
